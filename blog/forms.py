b8b980621ae9932a6ba974fc49eb9c7d5cc73c07
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2025-09-30 18:55:26 +0000

    Implantação Tailwindcss

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,19 @@ from .models import Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('nome', 'email', 'corpo')
+        fields = ['nome', 'email', 'corpo']
+        widgets = {
+            'nome': forms.TextInput(attrs={
+                'class': 'mt-1 block w-full rounded-md border border-slate-300 shadow-sm p-3 focus:border-blue-500 focus:ring focus:ring-blue-200',
+                'placeholder': 'Digite seu nome'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'mt-1 block w-full rounded-md border border-slate-300 shadow-sm p-3 focus:border-blue-500 focus:ring focus:ring-blue-200',
+                'placeholder': 'Digite seu e-mail'
+            }),
+            'corpo': forms.Textarea(attrs={
+                'class': 'mt-1 block w-full rounded-md border border-slate-300 shadow-sm p-3 focus:border-blue-500 focus:ring focus:ring-blue-200',
+                'placeholder': 'Escreva seu comentário...',
+                'rows': 4
+            }),
+        }
